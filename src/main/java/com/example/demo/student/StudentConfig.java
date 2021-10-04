a6f7d59af0777fbb92d367fcd1f447e7f85f0e6e
alfonz <aldelatorre@gbox.adnu.edu.ph>
2021-10-04 17:47:50 +0800

    HTTP client and Endpoints

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -22,25 +22,28 @@ public class StudentConfig {
             Student Alfonz = new Student(
                     "Alfonz",
                     "aldelatorre@gbox.adnu.edu.ph",
-                    LocalDate.of(2002, FEBRUARY, 22),
-                    19
+                    LocalDate.of(2002, FEBRUARY, 22)
+            );
+
+            Student Nikki = new Student(
+                    "Nikki",
+                    "Nikkidelatorre@gbox.adnu.edu.ph",
+                    LocalDate.of(1996, FEBRUARY, 22)
             );
 
             Student John = new Student(
                     "John",
                     "Johndelacruz@gbox.adnu.edu.ph",
-                    LocalDate.of(2003, FEBRUARY, 22),
-                    18
+                    LocalDate.of(2003, FEBRUARY, 22)
             );
 
             Student Betty = new Student(
                     "Betty",
                     "BettyMayn@gbox.adnu.edu.ph",
-                    LocalDate.of(2001, FEBRUARY, 22),
-                    20
+                    LocalDate.of(2001, FEBRUARY, 22)
             );
             repository.saveAll(
-                    Arrays.asList(Alfonz, John, Betty)
+                    Arrays.asList(Alfonz, Nikki, John, Betty)
             );
         };
     }
